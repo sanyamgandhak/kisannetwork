073d962e260c3343332022044e0cb8a26e26148e
Sayam Gandhak <gandhaksanyam@gmail.com>
2023-01-09 21:51:19 +0530

    port updated

--- server.js
+++ server.js
@@ -94,6 +94,6 @@ if (process.env.NODE_ENV === "production") {
   console.log("Mello");
 }
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log("server started "));
